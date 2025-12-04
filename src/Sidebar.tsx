6de5e308ef6009bc59e259d6bba14a2ba42fde0c
Callum Jones <callum.o.jones+git@protonmail.com>
2025-12-04 14:43:06 +1100

    Update visuals

--- src/Sidebar.tsx
+++ src/Sidebar.tsx
@@ -3,11 +3,11 @@ import { NavLink } from "react-router";
 export default function Sidebar() {
   return (
     <>
-      <div className="text-center flex flex-row gap-1 w-full justify-between items-center mb-8">
-        <NavLink to="/" className="hover:underline text-red-400">
+      <div className="text-center flex flex-row gap-1 w-full justify-center items-center mb-8 rounded-full">
+        {/* <NavLink to="/" className={({ isActive }) => `flex items-center p-2 px-4 rounded-full ${isActive ? 'dark:bg-red-200 dark:text-red-950 bg-red-100 text-red-900' : 'bg-neutral-700'}`}>
           /~
-        </NavLink>
-        <nav className="flex flex-row justify-center bg-white dark:bg-neutral-700 rounded-full">
+        </NavLink> */}
+        <nav className="flex flex-row justify-center bg-white dark:bg-neutral-700 rounded-full shadow-md">
           <NavLink
             to="/"
             className={({ isActive }) => `flex items-center p-2 px-4 rounded-full ${isActive ? 'dark:bg-red-200 dark:text-red-950 bg-red-100 text-red-900' : ''}`}
--- src/screens/ProjectScreen.tsx
+++ src/screens/ProjectScreen.tsx
@@ -6,6 +6,20 @@ import TerminalTyperSection from "./TerminalTyperSection";
 export default function ProjectScreen() {
   return (
     <>
+      <div className="border-b border-red-300 mb-2 pb-4 relative">
+        <h1
+          className="font-bold text-3xl flex items-center gap-1"
+        >
+          Projects
+        </h1>
+        <p className="mt-3">The following projects are detailed below:</p>
+        <ul role='list' className='list-decimal marker:text-red-900 marker:dark:text-red-400 ml-8 mt-2'>
+          <li>Leadlight</li>
+          <li>Coralia</li>
+          <li>Terminal Typer</li>
+          <li>FreeBSD Rootkit</li>
+        </ul>
+      </div>
       <LeadlightSection />
       <CoraliaSection />
       <TerminalTyperSection />
