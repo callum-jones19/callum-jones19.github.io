5500b5ace1f137839ce99c8428062c2f2d0b1bcb
Callum Jones <callum.o.jones+git@protonmail.com>
2025-12-19 00:26:22 +1100

    Fix build for second index.html

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,23 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        nested: resolve(__dirname, 'book/index.html'),
+      },
+    },
+  },
 })
